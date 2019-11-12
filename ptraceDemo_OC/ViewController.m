81d46a6aaea82ad8f06168d88568e614c809ea75
891650904 <891650904@qq.com>
2019-11-12 15:21:05 +0800

    提交使用ptrace来阻止lldb进行调试

--- ptraceDemo_OC/ViewController.m
+++ ptraceDemo_OC/ViewController.m
@@ -17,7 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    button.backgroundColor = [UIColor redColor];
+    [button addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
+- (void)buttonAction:(UIButton *)btn {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"弹框" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"a" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
--- ptraceDemo_OC/main.m
+++ ptraceDemo_OC/main.m
@@ -9,7 +9,36 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+#import <dlfcn.h>
+#import <sys/types.h>
+
+typedef int (*ptrace_ptr_t)(int _request,pid_t _pid,caddr_t _addr,int _data);
+
+#if !defined(PT_DENY_ATTACH)
+
+#define PT_DENY_ATTACH 31
+
+#endif
+
+
+void disable_gdb(){
+    
+    void* handle = dlopen(0, RTLD_GLOBAL|RTLD_NOW);
+    
+    ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
+    
+    ptrace_ptr(PT_DENY_ATTACH,0,0,0);
+    
+    dlclose(handle);
+}
+
 int main(int argc, char * argv[]) {
+    
+/**防止GDB挂起*/
+#ifndef DUBUG
+    disable_gdb();
+#endif
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
